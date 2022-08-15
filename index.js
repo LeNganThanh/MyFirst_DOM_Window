fa991a4e95fe49c40eca402e0b93245e663a01bd
Thanh Le <99728087+LeNganThanh@users.noreply.github.com>
2022-08-15 19:28:02 +0200

    getting the random number between 2 given numbers

--- index.js
+++ index.js
@@ -1,20 +1,56 @@
 "use strict";
-const randomNum = Math.floor(Math.random() * 10) + 1;
-const tempNum = window.prompt("Guess a number between 1 - 10");
+//Get random number between 2 given number
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+const randomNum = randomInt(1, 10);
+// const randomNum = Math.floor(Math.random() * 10) + 1;
+const tempNum = +prompt("Guess a number between 1 - 10");
 let count = 1;
 const checkRandomNum = function (num) {
-  if (Number(num) === randomNum) {
-    window.alert(`Success, the number was ${randomNum}! Attempts: ${count}`);
+  if (num === randomNum) {
+    alert(`Success, the number was ${randomNum}! Attempts: ${count}`);
     return;
-  } else if (Number(num) !== randomNum && count < 3) {
-    num = window.prompt("Wrong!!! Try again!");
+  } else if (num !== randomNum && count < 3) {
+    num = +prompt("Wrong!!! Try again!");
     count++;
     checkRandomNum(num);
-  } else if (Number(num) !== randomNum && count === 3) {
-    window.alert(
+  } else if (num !== randomNum && count === 3) {
+    alert(
       `Sorry, you failed to guess the number in three attempts. The number was ${randomNum}!`
     );
     return;
   }
 };
 checkRandomNum(tempNum);
+
+//Reload the page to replay or just exit.
+let playAgain = prompt("Would you like to play again? (y/n)");
+if (playAgain == "y") {
+  location.reload();
+} else {
+  alert("Thank you for playing!");
+}
+
+/* solution
+function play() {
+    let randomNum = Math.floor(Math.random() * 10) + 1;
+    console.log(randomNum);
+    let attempts = 1;
+    let guess = Number(prompt('Guess a number between 1 and 10'));
+
+    while (guess !== randomNum && attempts < 3) {
+        attempts += 1;
+        guess = Number(prompt('Wrong!!! Try again!'));
+    }
+
+    if (guess === randomNum) {
+        alert(`Success! The number was ${randomNum}! Attempts: ${attempts}`);
+    } else {
+        alert(`Sorry, you failed to guess the number in 3 attempts. The number was ${randomNum}`);
+    }
+
+}
+
+play(); 
+
+*/
